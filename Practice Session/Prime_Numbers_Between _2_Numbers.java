3eec9f170f7aa50a637c82ef9ef0a1f4e2dbd2f7
amit951 <42816005+amit951@users.noreply.github.com>
2021-04-17 18:05:16 +0530

    Add files via upload

---
+++ Practice Session/Prime_Numbers_Between _2_Numbers.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+class Abc {
+
+	public static void main(String args[]) {
+		int start, end;
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter Start Number");
+		start = s.nextInt();
+		System.out.println("Enter end Number");
+		end = s.nextInt();
+		for (int i = start; i < end; i++) {
+			if (isPrime(i)) {
+				System.out.println(i);
+			}
+		}
+	}
+
+	public static Boolean isPrime(int n) {
+
+		if (n <= 1) {
+			return false;
+		}
+
+		for (int i = 2; i < Math.sqrt(n); i++) {
+			if (n % i == 0) {
+				return false;
+			}
+			// else
+
+		}
+
+		return true;
+	}
+}

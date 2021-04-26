3f09db8d093f174d4e302ffa37cdb532fbef08c1
amit951 <42816005+amit951@users.noreply.github.com>
2021-04-26 22:32:39 +0530

    second commit

--- Spring_ORM/src/main/java/com/spring_orm/mySpringORM/dao/Student_Dao.java
+++ Spring_ORM/src/main/java/com/spring_orm/mySpringORM/dao/Student_Dao.java
@@ -1,5 +1,7 @@
 package com.spring_orm.mySpringORM.dao;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 
 import org.springframework.orm.hibernate5.HibernateTemplate;
@@ -18,7 +20,38 @@ public class Student_Dao {
 		Integer i=(Integer)this.hibernateTemplate.save(s);
 		return i;
 	}
-
+	
+	//get single data
+	public Student getStudent(int student_id)
+	{
+		return this.hibernateTemplate.get(Student.class,student_id);
+	}
+	
+	//get all object/data
+	public List<Student> getAllStudent()
+	{
+		return this.hibernateTemplate.loadAll(Student.class);
+	}
+	
+	
+	//delete data
+	@Transactional
+	public void deleteStudent(int student_id)
+	{
+		
+		Student student=this.hibernateTemplate.get(Student.class,student_id);
+		this.hibernateTemplate.delete(student);
+	}
+	
+	
+	//update data
+	@Transactional
+	public void updateStudent(int student_id)
+	{
+		this.hibernateTemplate.update(student_id);
+	}
+	
+	
 	public HibernateTemplate getHibernateTemplate() {
 		return hibernateTemplate;
 	}
